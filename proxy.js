3e737485b34426e8f22f5dc124b3a4f313adcdef
tianfang <tianfang07@outlook.com>
2024-07-27 00:22:47 +0800

    a

--- proxy.js
+++ proxy.js
@@ -9,7 +9,7 @@ module.exports = (req, res) => {
   // 这里使用 backend 主要用于区分 vercel serverless 的 api 路径
   // target 替换为你跨域请求的服务器 如： http://gmall-h5-api.atguigu.cn
   if (req.url.startsWith('/api')) {
-    target = 'http://192.168.101.174:8080'
+    target = 'http://192.168.101.174'
   }
   // 创建代理对象并转发请求
   createProxyMiddleware({
@@ -18,7 +18,7 @@ module.exports = (req, res) => {
     pathRewrite: {
       // 通过路径重写，去除请求路径中的 `/api`
       // 如果开启了,那么 /api/user/login 将被转发到 http://gmall-h5-api.atguigu.cn/user/login
-      // '^/api/': '/',
+      '^/api/': '/',
     },
   })(req, res)
 }
